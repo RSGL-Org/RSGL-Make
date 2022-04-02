92590633951af465f1f9cd398ed5e471239e9124
ColleagueRiley <ColleagueRiley@gmail.com>
2022-04-02 12:13:55 -0400

    added no file error

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,13 @@
 #include "../include/tokens.hpp"
 
 int main(int argc, char** argv){
+    if (file == ""){ std::cout << "No RMakefile file found or empty file found" << std::endl; return 0;}
     std::vector<std::string> calls;
     for (int i=0; i < argc; i++) calls.insert(calls.end(),std::string(argv[i]));
     std::vector<Token> tokens = tokenize(calls);
     bool comp=true;
     std::string source, args, libs, output, compiler;
-    for (int i=0; i < tokens.size(); i++){
+    for (int i=0; i < tokens.size() && comp; i++){
         switch(tokens.at(i).t){
             case src: source += tokens.at(i).data + " "; break;
             case arg: args += tokens.at(i).data + " ";  break;
@@ -14,7 +15,7 @@ int main(int argc, char** argv){
             case out: output += tokens.at(i).data + " "; break;
             case gxx: compiler += tokens.at(i).data + " "; break;
             case nocomp: comp=false; break;
-        }
+        } 
     }
     if (comp){
         std::string cmd = compiler + source + args + libs + "-o " + output;
